49ae17ac443afff7fe9c620162cc1e914309bfc5
Ahmed Omer <ahmed.omer@confiz.com>
2015-06-16 21:41:25 +0500

    Initial Setup

---
+++ BRYG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRYG
+//
+//  Created by Ahmed Omer on 16/06/2015.
+//  Copyright (c) 2015 Ahmed Omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BRYG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRYG
+//
+//  Created by Ahmed Omer on 16/06/2015.
+//  Copyright (c) 2015 Ahmed Omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
